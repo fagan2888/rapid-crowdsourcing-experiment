1e03362f19ac1f0b033aac843c01f981aff64339
Micah Smith <micahjsmith@gmail.com>
2017-04-12 16:38:09 -0400

    Add logic to display a single doc

---
+++ web/data/links.js
@@ -0,0 +1,29 @@
+var links = {
+    "dogs" :
+        { 
+            "positive" : [
+                    "http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg"
+                ],
+            "negative" : [
+                    "https://www.cgstud.io/imgd/s/5581863eccff75eb7c8b4567/5938.jpg"
+                ]
+        },
+    "motorcycles" :
+        {
+            "positive" : [
+                    "https://theridingcenter.com/wp-content/uploads/2015/05/bigstock-Man-Riding-A-Motorcycle-89211392.jpg"
+                ],
+            "negative" : [
+                    "https://polaris.hs.llnwd.net/o40/vic/2017/img/motorcycles/my17-motorcycles-page/cruisers-en-us.png"
+                ]
+        },
+    "breakfast" :
+        {
+            "positive" : [
+                    "http://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2013/5/10/0/TS-161146814_family-with-small-kids-eat-in-kitchen-breakfast.jpg.rend.hgtvcom.1280.960.jpeg"
+                ],
+            "negative" : [
+                    "http://store.hp.com/wcsstore/hpusstore/Treatment/dsnew-printers-drawer-1-3.jpg"
+                ]
+        }
+}
--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,31 @@
+var c = {};
+
 $(document).ready(function() {
-  console.log("ready");
+  c.playing = false;
+  downloadImages();
 });
+
+$(document).on("click", "#btn_play", function(evt) {
+  console.log("about to play images");
+  playImages();
+});
+
+// ------------------------------------------------------------------------------
+// Mess with images
+
+function downloadImages(){
+  c.links = links;
+}
+
+function playImages(){
+  // create an array of `n` images
+  // set a timer to fire every `t` milleseconds
+  // when timer fires, change src of image
+  // at end, clear panel
+  $( "#image_panel_image" ).attr({
+    "src" : c.links.dogs.positive[0]
+  });
+}
+
+function clearImages(){
+}
