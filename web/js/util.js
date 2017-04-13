d45e5356e9d5ad38209040d941526238e04bee07
Micah Smith <micahjsmith@gmail.com>
2017-04-12 23:35:36 -0400

    Add utils

---
+++ web/js/util.js
@@ -0,0 +1,26 @@
+// Standard Normal variate using Box-Muller transform.
+function randn_bm() {
+    var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+    var v = 1 - Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+// String format
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+// Call a function N times
+function callNTimes(func, num, delay) {
+     if (!num) return;
+     func();
+     setTimeout(function() { callNTimes(func, num - 1, delay); }, delay);
+}
