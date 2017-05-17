6d78cfb43f6911c13df43be3b6241355b996d7bc
Micah Smith <micahjsmith@gmail.com>
2017-05-16 20:02:08 -0400

    Update RS3 analysis

--- resources/log/extract_log.py
+++ resources/log/extract_log.py
@@ -2,26 +2,30 @@
 
 import json
 import requests
+import fire
 
 # setup
 sheetsu_id = "1da100d38704"
 sheetsu_url = "https://sheetsu.com/apis/v1.0/{}".format(sheetsu_id)
-filename_csv = "data.csv"
+flds = ["id", "interface", "source", "task", "timestamp", "uuid", "value"]
 
+def download(filename_csv="data.csv"):
 # open file for writing
-with open(filename_csv, "w") as g:
-    g.write("id,interface,source,task,timestamp,uuid,value\n")
+    with open(filename_csv, "w") as g:
+        g.write(",".join(flds) + "\n")
 
-    # download data
-    response = requests.get(sheetsu_url)
+        # download data
+        response = requests.get(sheetsu_url)
 
-    if response.ok:
-        data = response.json()
-        for line in data:
-            entry = json.loads(line["dump"])
-            for d in entry:
-                g.write("{},{},{},{},{},{},{}\n".format(
-                    d["id"], d["interface"], d["source"], d["task"],
-                    d["timestamp"], d["uuid"], d["value"]))
-    else:
-        print("Failed to download ({})".format(response.reason))
+        if response.ok:
+            data = response.json()
+            for line in data:
+                entry = json.loads(line["dump"])
+                for d in entry:
+                    g.write("{},{},{},{},{},{},{}\n".format(
+                        *[d[f] for f in flds]))
+        else:
+            print("Failed to download ({})".format(response.reason))
+
+if __name__ == "__main__":
+    fire.Fire(download)
